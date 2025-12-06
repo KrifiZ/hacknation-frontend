41754a04e46cf2e6f05ba01e4a1ca23a3d37f989
Bartosz Zakrzewski <78502440+BartekZakrzewski@users.noreply.github.com>
2025-12-06 20:36:59 +0100

    Created DataGrid component

---
+++ src/features/datagrid/view-model/data-grid-view.model.ts
@@ -0,0 +1,159 @@
+export const DataColumns = [
+    {
+        header: 'Część budżetowa',
+        type: 'numeric'
+    },
+    {
+        header: 'Dział',
+        type: 'numeric'
+    },
+    {
+        header: 'Rozdział',
+        type: 'numeric'
+    },
+    {
+        header: 'Paragraf',
+        type: 'numeric'
+    },
+    {
+        header: 'Źródło finansowania',
+        type: 'numeric'
+    },
+    {
+        header: 'Grupa wydatków',
+        type: 'text'
+    },
+    {
+        header: 'Budżet zadaniowy (w pełnej szczegółowości)',
+        type: 'text'
+    },
+    {
+        header: 'Budżet zadaniowy (nr funkcji, nr zadania)',
+        type: 'text'
+    },
+    {
+        header: 'Nazwa programu/projektu',
+        type: 'text'
+    },
+    {
+        header: 'Nazwa komórki organizacyjnej',
+        type: 'text'
+    },
+    {
+        header: 'Plan WI',
+        type: 'text'
+    },
+    {
+        header: 'Dysponent środków',
+        type: 'text'
+    },
+    {
+        header: 'Budżet',
+        type: 'text'
+    },
+    {
+        header: 'Nazwa zadania',
+        type: 'text'
+    },
+    {
+        header: 'Szczegółowe uzasadnienie realizacji zdania',
+        type: 'text'
+    },
+    {
+        header: 'Przeznaczenie wydatków wg obszaru działalności: cyberbezpieczeństwo/sztuczna inteligencja/koszty funkcjonowania/inne (wpisać jakie?)',
+        type: 'text'
+    },
+    {
+        header: 'Potrzeby finansowe na 2026 rok',
+        type: 'numeric'
+    },
+    {
+        header: 'Limit wydatków na rok 2026',
+        type: 'numeric'
+    },
+    {
+        header: 'Kwota na realizację zadań w 2026 roku, która nie została zabezpieczona w limicie (kol. 17-kol. 18)',
+        type: 'numeric'
+    },
+    {
+        header: 'Kwota zawartej umowy/wniosku o udzielenie zamówienia publicznego',
+        type: 'numeric'
+    },
+    {
+        header: 'Nr umowy/nr wniosku o udzielenie zamówienia publicznego',
+        type: 'text'
+    },
+    {
+        header: 'Potrzeby finansowe na 2027 rok',
+        type: 'numeric'
+    },
+    {
+        header: 'Limit wydatków na rok 2027',
+        type: 'numeric'
+    },
+    {
+        header: 'Kwota na realizację zadań w 2026 roku, która nie została zabezpieczona w limicie (kol. 22-kol. 23)',
+        type: 'numeric'
+    },
+    {
+        header: 'Kwota zawartej umowy/wniosku o udzielenie zamówienia publicznego',
+        type: 'numeric'
+    },
+    {
+        header: 'Nr umowy/nr wniosku o udzielenie zamówienia publicznego',
+        type: 'text'
+    },
+    {
+        header: 'Potrzeby finansowe na 2028 rok',
+        type: 'numeric'
+    },
+    {
+        header: 'Limit wydatków na rok 2028',
+        type: 'numeric'
+    },
+    {
+        header: 'Kwota na realizację zadań w 2026 roku, która nie została zabezpieczona w limicie (kol. 27-kol. 28)',
+        type: 'numeric'
+    },
+    {
+        header: 'Kwota zawartej umowy/wniosku o udzielenie zamówienia publicznego',
+        type: 'numeric'
+    },
+    {
+        header: 'Nr umowy/nr wniosku o udzielenie zamówienia publicznego',
+        type: 'text'
+    },
+    {
+        header: 'Potrzeby finansowe na 2029 rok',
+        type: 'numeric'
+    },
+    {
+        header: 'Limit wydatków na rok 2029',
+        type: 'numeric'
+    },
+    {
+        header: 'Kwota na realizację zadań w 2026 roku, która nie została zabezpieczona w limicie (kol. 32-kol. 33)',
+        type: 'numeric'
+    },
+    {
+        header: 'Kwota zawartej umowy/wniosku o udzielenie zamówienia publicznego',
+        type: 'numeric'
+    },
+    {
+        header: 'Nr umowy/nr wniosku o udzielenie zamówienia publicznego',
+        type: 'text'
+    },
+    {
+        header: 'W przypadku dotacji - z kim zawarta umowa/planowana do zawarcia umowa',
+        type: 'text'
+    },
+    {
+        header: 'Podstawa prawna udzielenia dotacji',
+        type: 'text'
+    },
+    {
+        header: 'Uwagi',
+        type: 'text'
+    },
+];
+
--- src/pages/DepartamentItem.tsx
+++ src/pages/DepartamentItem.tsx
@@ -1,30 +1,13 @@
 import { Link } from 'react-router'
-import 'react-data-grid/lib/styles.css';
-import { DataGrid, type Column } from 'react-data-grid';
+import DataGridComponent from '../features/datagrid/DataGridComponent'
 
-interface Row {
-  id: number;
-  title: string;
-}
-
-const columns: readonly Column<Row>[] = [
-  { key: 'id', name: 'ID' },
-  { key: 'title', name: 'Title' }
-];
-
-const rows: readonly Row[] = [
-  { id: 0, title: 'Example' },
-  { id: 1, title: 'Demo' }
-];
 
 export function DepartamentItem() {
-
-
     return (
         <div>
             <h1>Current budget</h1>
             <Link to={'/department'}>Back to department</Link>
-            <DataGrid columns={columns} rows={rows} />
+            <DataGridComponent />
         </div>
     )
 
